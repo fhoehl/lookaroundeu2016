481316ce5a5418a5bfbd1fa4f6eb72dffd1cdbfe
Francois Hoehl <francoishoehl@gmail.com>
2016-06-15 02:37:14 +0200

    Update data and import tool
    The import tool has been changed to read the new csv layout. It also now
    do simple formatting for currency and title.

--- csv2json.py
+++ csv2json.py
@@ -8,6 +8,17 @@ Parse data from csv database and output json for the front-end.
 import argparse
 import csv
 import json
+import sys
+import locale
+import string
+
+def urlify(url):
+    if not url.startswith('http'):
+        return 'http://%s' % url
+    return url
+
+def moneyfy(amount):
+    return locale.currency(amount, grouping=True).split('.')[0]
 
 def parse_csv(file_path):
     '''Parse file '''
@@ -17,15 +28,21 @@ def parse_csv(file_path):
     with open(file_path, 'r') as csv_file:
         reader = csv.reader(csv_file)
         reader.next()
-        for row in reader:
-            locations[row[0]] = dict(
-                t=row[0], #venue title
-                p=row[2].strip(), #lat,lng
-                i=row[1].strip(), #address
-                f=int(row[3].replace('£', '').replace(',', '') or 0), #funding
-                s=row[4].strip(), #source
-                d=row[5].strip(), #description
-            )
+        for i, row in enumerate(reader):
+            if row[7] is 'Y':
+                if row[0].endswith('...'):
+                    print >> sys.stderr, "Line %i: %s" % (i, row[0])
+                try:
+                    locations[string.capwords(row[0])] = dict(
+                        t=string.capwords(row[0]), #venue title
+                        p=row[2].strip(), #lat,lng
+                        i=row[3].strip(), #address
+                        f=moneyfy(int(row[1] or 0)), #funding
+                        s=urlify(row[4].strip()), #source
+                        d=row[5].strip(), #description
+                    )
+                except ValueError, e:
+                    print >> sys.stderr, "Error parsing row %i: %s" % (i, e)
 
     print json.dumps(locations, separators=(',', ':'))
 
@@ -34,6 +51,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('csv_path', help='CSV file path')
     args = parser.parse_args()
+
+    locale.setlocale(locale.LC_ALL, locale='en_GB')
 
     parse_csv(args.csv_path)
 
