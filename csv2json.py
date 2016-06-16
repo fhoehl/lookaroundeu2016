24587a1c448ce33da77d4d807d5bf8542daa3d8b
Francois Hoehl <francoishoehl@gmail.com>
2016-06-16 17:12:13 +0200

    Update data import tool
    The schema of the spreadsheet changed to include currency
    
    Refactor script to have a clearer unpacking of item in venues row

--- csv2json.py
+++ csv2json.py
@@ -11,49 +11,86 @@ import json
 import sys
 import locale
 import string
+from collections import namedtuple
+
+from babel.numbers import format_currency
+
+VENUE_TITLE = 0
+VENUE_GEO = 3
+VENUE_ADDRESS = 4
+VENUE_FUNDING = 2
+VENUE_FUNDING_CURRENCY = 1
+VENUE_SOURCE = 5
+VENUE_DESCRIPTION = 6
+VENUE_PUBLISH = 8
+
+Row = namedtuple('Row', [
+    'title',
+    'geo', 'address',
+    'funding', 'currency',
+    'source',
+    'publish',
+    'description'])
 
 def urlify(url):
+    '''Append http:// in front of url'''
     if not url.startswith('http'):
         return 'http://%s' % url
     return url
 
-def moneyfy(amount):
-    return locale.currency(amount, grouping=True).split('.')[0]
+def moneyfy(currency, amount):
+    '''Format amount of money according to a locale'''
+    if currency.lower() in ('€', 'eur'):
+        return format_currency(int(amount), 'EUR', locale='en_GB').split('.')[0]
+    elif currency.lower() in ('£', 'gbp'):
+        return format_currency(int(amount), 'GBP', locale='en_GB').split('.')[0]
+    return amount
+
+def maprow(line_number, row):
+    '''Extract data from a row'''
+    try:
+        return Row(
+            title=string.capwords(row[VENUE_TITLE]),
+            geo=row[VENUE_GEO].strip(),
+            address=row[VENUE_ADDRESS].strip(),
+            funding=moneyfy(row[VENUE_FUNDING_CURRENCY], int(row[VENUE_FUNDING] or 0)),
+            source=urlify(row[VENUE_SOURCE].strip()),
+            description=row[VENUE_DESCRIPTION].strip(),
+            currency=row[VENUE_FUNDING_CURRENCY],
+            publish=True if row[VENUE_PUBLISH] is 'Y' else False)
+    except ValueError, exception:
+        print >> sys.stderr, "Error parsing row %i: %s" % (line_number, exception)
+        return None
 
 def parse_csv(file_path):
-    '''Parse file '''
+    '''Parse file'''
 
     locations = dict()
 
     with open(file_path, 'r') as csv_file:
         reader = csv.reader(csv_file)
         reader.next()
-        for i, row in enumerate(reader):
-            if row[7] is 'Y':
-                if row[0].endswith('...'):
-                    print >> sys.stderr, "Line %i: %s" % (i, row[0])
-                try:
-                    locations[string.capwords(row[0])] = dict(
-                        t=string.capwords(row[0]), #venue title
-                        p=row[2].strip(), #lat,lng
-                        i=row[3].strip(), #address
-                        f=moneyfy(int(row[1] or 0)), #funding
-                        s=urlify(row[4].strip()), #source
-                        d=row[5].strip(), #description
-                    )
-                except ValueError, e:
-                    print >> sys.stderr, "Error parsing row %i: %s" % (i, e)
+        for line_number, row in enumerate(reader):
+            venue = maprow(line_number, row)
+            if venue and venue.publish:
+                if venue.title.endswith('...'):
+                    print >> sys.stderr, "Line %i: %s" % (line_number, row[0])
 
-    print json.dumps(locations, separators=(',', ':'))
+                locations[venue.title] = dict(
+                    t=venue.title,
+                    p=venue.geo,
+                    i=venue.address,
+                    f=venue.funding,
+                    s=venue.source,
+                    d=venue.description,)
+
+        print json.dumps(locations, separators=(',', ':'))
 
 def main():
     '''main'''
     parser = argparse.ArgumentParser()
     parser.add_argument('csv_path', help='CSV file path')
     args = parser.parse_args()
-
-    locale.setlocale(locale.LC_ALL, locale='en_GB')
-
     parse_csv(args.csv_path)
 
 if __name__ == '__main__':
